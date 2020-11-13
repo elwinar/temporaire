a20fdb2ca188860306e7d2b1be3aa5b0a1cc9c68
Marwan <marwan.mezroui.pro@gmail.com>
2020-11-13 01:48:04 +0100

    Add viewLogin's structure

---
+++ views/viewLogin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> TeamDash - Login  </title>
+        <link rel="icon" type="image/png" href="../src/images/logo.png">
+    </head>    
+    <body>
+        <img id="wave" src="../src/images/wave.png">
+        <div id="login" class="container">
+            <div class="img">
+                <img src="../src/images/login_img.svg">
+            </div>
+            <div class="login-container">
+                <form action="viewLogin.php">
+                    <div class="avatar_container">
+                        <img src="../src/images/male_avatar.svg">
+                    </div>
+                    <div class="container">
+                       <h5> Email </h5>
+                       <input class="input" type="email">
+
+                        <h5> Password </h5>
+                        <input class="input" type="password"> 
+                    </div> 
+                    <a href="#"> Forgot Password ? </a>
+                    <input type="submit" class="btn" value="Login"> 
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
